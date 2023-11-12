9093f11e1f040588a5c44402d97373071e24eebe
He Liu <liulonnie@gmail.com>
2023-11-11 16:37:36 -0800

    add support for docker version querying (#2)
    also upgrade golang libs

---
+++ dock/version.go
@@ -0,0 +1,20 @@
+package dock
+
+// VersionInfo saves the response of the `/version` docker endpoint.
+type VersionInfo struct {
+	Version       string `json:"Version"`
+	APIVersion    string `json:"ApiVersion"`
+	OS            string `json:"Os"`
+	KernelVersion string `json:"KernelVersion"`
+	GoVersion     string `json:"GoVersion"`
+	Arch          string `json:"Arch"`
+}
+
+// Version returns the version info of the docker service.
+func Version(c *Client) (*VersionInfo, error) {
+	info := new(VersionInfo)
+	if err := c.jsonGet("version", nil, info); err != nil {
+		return nil, err
+	}
+	return info, nil
+}
